b8683c766210d3dae89f004fedbb00b828cb66ca
dmitriy <kim.dm@sberbank.ru>
2020-04-19 18:59:44 +0300

    Updates

---
+++ precache-manifest.96b753e46c5a99bf7b19f7f1f64c0341.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d9fa04b846644105fc6c096e56a4cf6",
+    "url": "/coronaStats/index.html"
+  },
+  {
+    "revision": "fe67c89401cdca4f1b1a",
+    "url": "/coronaStats/static/css/main.6f2af6e2.chunk.css"
+  },
+  {
+    "revision": "823f15ab2fee2a534edd",
+    "url": "/coronaStats/static/js/2.51b0c9f5.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/coronaStats/static/js/2.51b0c9f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe67c89401cdca4f1b1a",
+    "url": "/coronaStats/static/js/main.10b0f727.chunk.js"
+  },
+  {
+    "revision": "1e817a069dbd186903e7",
+    "url": "/coronaStats/static/js/runtime-main.4834eb38.js"
+  }
+]);
